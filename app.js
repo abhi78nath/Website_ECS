93e4075b76c6dfacea5e550777f4647c86ebf888
Bikramjit <bikdev07@gmail.com>
2021-12-29 11:33:08 +0530

    1st commit

---
+++ app.js
@@ -0,0 +1,66 @@
+const express=require("express");
+const ejs=require("ejs");
+const app=express();
+
+app.set('view engine','ejs');
+
+app.use(express.static("public"));
+
+app.get("/",function(req,res){
+    res.render("home.ejs");
+});
+
+app.get("/Resources",function(req,res){
+    res.render("Resources.ejs");
+});
+
+app.get("/Members",function(req,res){
+    res.render("Members.ejs");
+});
+
+app.get("/Developers",function(req,res){
+    res.render("Developers.ejs");
+});
+
+app.get("/gallery",function(req,res){
+    res.render("gallery.ejs");
+});
+
+app.get("/1stsem",function(req,res){
+    res.render("1stsem.ejs");
+});
+
+app.get("/2ndsem",function(req,res){
+    res.render("2ndsem.ejs");
+});
+
+app.get("/3rdsem",function(req,res){
+    res.render("3rdsem.ejs");
+});
+
+app.get("/4thsem",function(req,res){
+    res.render("4thsem.ejs");
+});
+
+app.get("/5thsem",function(req,res){
+    res.render("5thsem.ejs");
+});
+
+app.get("/6thsem",function(req,res){
+    res.render("6thsem.ejs");
+});
+
+app.get("/7thsem",function(req,res){
+    res.render("7thsem.ejs");
+});
+
+app.get("/8thsem",function(req,res){
+    res.render("8thsem.ejs");
+});
+
+
+
+
+app.listen(process.env.PORT || 3000,function(){
+    console.log("server is running");
+});
---
+++ public/js/slider.js
@@ -0,0 +1,30 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides2");
+  var dots2 = document.getElementsByClassName("dot2");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots2.length; i++) {
+      dots2[i].className = dots2[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots2[slideIndex-1].className += " active";
+
+  //setTimeout(showSlides(slideIndex),3000);
+}

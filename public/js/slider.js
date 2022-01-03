c99c3cf85f01cbd8dd0ad17f2f2223674c1258a9
Bikramjit <bikdev07@gmail.com>
2022-01-03 19:41:42 +0530

    gallery slider animation

--- public/js/slider.js
+++ public/js/slider.js
@@ -1,5 +1,5 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+var slideIndex = 0;
+showSlides();
 
 // Next/previous controls
 function plusSlides(n) {
@@ -11,7 +11,7 @@ function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
-function showSlides(n) {
+/*function showSlides() {
   var i;
   var slides = document.getElementsByClassName("mySlides2");
   var dots2 = document.getElementsByClassName("dot2");
@@ -26,5 +26,28 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots2[slideIndex-1].className += " active";
 
-  //setTimeout(showSlides(slideIndex),3000);
+  setTimeout(showSlides,3000);
+}*/
+
+//new-----
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides2");
+  var dots = document.getElementsByClassName("dot2");
+
+  //arrow,dot
+ // if (n > slides.length) {slideIndex = 1}
+  //if (n < 1) {slideIndex = slides.length}
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 6000); // Change image every 2 seconds
 }
